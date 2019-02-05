14104ddfd1d6229e3eb920c0b28acc420a45e394
Tamas K. Papp <tkpapp@gmail.com>
2019-02-05 13:22:07 +0100

    push image when done

---
+++ push-script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+- set -e
+- if [ "$TRAVIS_BRANCH" != "master" ]
+then
+    echo "Not on master, not pushing Docker image."
+    exit 0
+fi
+
+echo "Pushing Docker image."
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+docker push $DOCKER_USER/$IMAGE
